8a7195d5f8fe3bfd84fde16ea40eee28a84be827
Aashish Jha <jhaashish.ajha@gmail.com>
2025-07-01 17:04:11 +0530

    Doubley LL reversed succesfully

---
+++ LinkedList/ReverseADLL.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std ; 
+
+
+class Node{
+
+    public:
+    int data ; 
+    Node* back ;
+    Node* next ;
+
+    public:
+    Node(int data1 , Node* back1 , Node* next1){
+        data = data1;
+        back= back1;
+        next= next1;
+    }
+
+    Node(int data1){
+        data= data1;
+        back=nullptr;
+        next=nullptr;
+    }
+
+
+};
+
+
+Node* ArrayToDoublyLL(vector<int> vect){
+    Node* head = new Node(vect[0]);
+    Node* prev = head ;
+
+    for(int i = 1 ; i<vect.size();i++){
+        Node* newNode = new Node(vect[i],prev,nullptr);
+        prev->next=newNode;
+        prev = newNode;
+    }
+
+    return head ;
+}
+
+
+Node* ReverseADLL(Node* head ){
+    Node* prev = nullptr;
+    Node* current = head ;
+
+    while(current!=NULL){
+        prev=current->back;
+        current->back= current->next;
+        current->next=prev;
+        current=current->back ;
+
+
+    }
+
+     if (prev != nullptr)
+        head = prev->back;
+
+    return head;
+   
+}
+
+
+
+void print(Node* head){
+    while(head!=nullptr){
+        cout<<head->data<<" <-->";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+
+int main(){
+
+vector<int> vect = {15,2,3,4,5,6};
+
+Node* head = ArrayToDoublyLL(vect);
+print(head);
+
+Node* reverseHead = ReverseADLL(head);
+print(reverseHead);
+
+}
