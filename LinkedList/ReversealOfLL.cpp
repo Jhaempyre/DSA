9de1b44ac9aa4343bf4090e8b33586bba68d6ae5
Aashish Jha <jhaashish.ajha@gmail.com>
2025-07-02 13:50:39 +0530

    Added some more reversal techniques

--- LinkedList/ReversealOfLL.cpp
+++ LinkedList/ReversealOfLL.cpp
@@ -75,9 +75,78 @@ Node* ReverseTheLL(Node* head ){
         
 }
 
+//well it can be done via recursion and also a single pass method 
+
+
+Node* reverseLinkedList(Node* head) {
+    // Base case:
+    // If the linked list is empty or has only one node,
+    // return the head as it is already reversed.
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    
+    // Recursive step:
+    // Reverse the linked list starting 
+    // from the second node (head->next).
+    Node* newHead = reverseLinkedList(head->next);
+    
+    // Save a reference to the node following
+    // the current 'head' node.
+    Node* front = head->next;
+    
+    // Make the 'front' node point to the current
+    // 'head' node in the reversed order.
+    front->next = head;
+    
+    // Break the link from the current 'head' node
+    // to the 'front' node to avoid cycles.
+    head->next = NULL;
+    
+    // Return the 'newHead,' which is the new
+    // head of the reversed linked list.
+    return newHead;
+}
 
 
 
+// Function to reverse a linked list (single pass)
+// using the 3-pointer approach
+Node* reverseLinkedList(Node *head)
+{
+   // Initialize'temp' at
+   // head of linked list
+   Node* temp = head;  
+   
+   // Initialize pointer 'prev' to NULL,
+   // representing the previous node
+   Node* prev = NULL;  
+   
+   // Traverse the list, continue till
+   // 'temp' reaches the end (NULL)
+   while(temp != NULL){  
+       // Store the next node in
+       // 'front' to preserve the reference
+       Node* front = temp->next;  
+       
+       // Reverse the direction of the
+       // current node's 'next' pointer
+       // to point to 'prev'
+       temp->next = prev;  
+       
+        // Move 'prev' to the current
+        // node for the next iteration
+       prev = temp;  
+       
+        // Move 'temp' to the 'front' node
+        // advancing the traversal
+       temp = front; 
+   }
+   
+   // Return the new head of
+   // the reversed linked list
+   return prev;  
+}
 
 void print(Node* head ){
     while(head!=nullptr){
